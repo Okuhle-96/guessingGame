bcb83cb88e3773234d65ad09eda99412560cad0d
Okuhle-96 <54100625+Okuhle-96@users.noreply.github.com>
2022-02-22 16:46:37 +0200

    Update script.js

--- script.js
+++ script.js
@@ -7,15 +7,30 @@ var number = Math.ceil((Math.random() * 100));
 function guessRandomNumber(){
     if(guess.value < number){
         message.innerHTML = "Your guess is too low.";
+        setTimeout(function(){
+            message.innerHTML = "";
+         }, 2000);
     }else if(guess.value > number) {
         message.innerHTML = "Your guess is too high.";
+        setTimeout(function(){
+            message.innerHTML = "";
+         }, 2000);
     }else {
-        message.innerHTML = `Correct, the secret number is ${number}`;
+        message.innerHTML = `Correct, the secret number is ${number} ! ! !`;
+        setTimeout(function(){
+            message.innerHTML = "";
+         }, 5000);
     }
+
+    return number;
     
-   setTimeout(function() {
-  message.innerHTML = "";
-}, 1500);
-
-
 }guessBtn.addEventListener("click", guessRandomNumber)
+
+function newGame(){
+    if(mesagge = "Correct, the secret number is ${number} ! ! !`"){
+        setTimeout(function(){
+          message.innerHTML = "Begin New Game";
+       }, 3000);
+        
+    }
+}
